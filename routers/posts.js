f70a6bb7036fcfcb354831044b522a40d826ae8c
EnricoGenovese <enricogeno@libero.it>
2024-12-03 14:54:37 +0100

    test get single item (show);

--- routers/posts.js
+++ routers/posts.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const posts = require("../data/database.js");
 
+// Index - Get all posts: localhost:3000/posts
 router.get("/", (req, res) => {
     const postName = req.query.name;
     //
@@ -25,7 +26,25 @@ router.get("/", (req, res) => {
         }
     }
     res.json(response);
-})
+});
+
+// Show - Get one post by id
+router.get("/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+    //
+    if (post) {
+        res.json({
+            success: true,
+            post,
+        });
+    } else {
+        res.json({
+            success: false,
+            messegge: "Post not found!"
+        });
+    }
+});
 
 
 // Mid point
