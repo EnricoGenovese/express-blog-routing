66a601587f87db4d3931535f795f7884f7f75ac6
EnricoGenovese <enricogeno@libero.it>
2024-12-03 15:16:36 +0100

    test destroy; add response for not existing posts;

--- routers/posts.js
+++ routers/posts.js
@@ -1,3 +1,18 @@
+/*
+### Esercizio
+Usando l'array dei post con le relative immagini, creare un file di routing (routers/posts.js) che conterrà le rotte necessarie per l'entità post.
+All'interno creare le rotte per le operazioni CRUD (Index, Show, Create, Update e Delete)
+Tutte le risposte saranno dei testi che confermeranno l’operazione che il server deve eseguire, secondo le convenzioni REST.
+Ad esempio:
+Se viene chiamata /posts col verbo GET ci aspettiamo “Lista dei post”;
+Se viene chiamato /posts/1 col verbo DELETE ci aspettiamo “Cancellazione del post 1”
+e via dicendo… 
+Registrare il router dentro app.js con il prefisso /posts.
+### Bonus
+- Provare a restituire la lista dei post dalla rotta index, in formato json
+- Provare a restituire un singolo post dalla rotta show, sempre in formato json
+*/
+
 const express = require("express");
 const router = express.Router();
 
@@ -55,15 +70,40 @@ router.post("/", (req, res) => {
 // Update - Change an item fully
 
 router.put("/:id", (req, res) => {
-    res.send("Item fully modified");
-})
+    const id = req.params.id;
+    const post = posts.find((post) => post.id === id);
+    if (post) {
+        res.send("Item fully modified");
+    } else {
+        res.send("Cannot update what doe not exist")
+    }
+});
 
 // Modify - Modify partially an item
 
 router.patch("/:id", (req, res) => {
-    res.send("Item patched")
+    const id = req.params.id;
+    const post = posts.find((post) => post.id === id);
+    if (post) {
+        res.send("Item patched")
+    } else {
+        res.send("Cannot patch what does not exist")
+    }
+
 })
 
+// Destroy - Deletes an element
+
+router.delete("/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+    if (post) {
+        res.send(`Item ${id} destroyed`)
+    } else {
+        res.send("Cannot destroy what does not exist")
+    }
+
+})
 
 // Mid point
 module.exports = router;
